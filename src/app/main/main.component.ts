0abf232789670b642741e1a0b8a50fed1fd6a060
Victor Manuel Avila Tamayo <skarnanotherblackwinter@gmail.com>
2024-07-13 15:22:47 -0400

    feat: Add actions, header, and tap components
    This commit adds the actions, header, and tap components to the application. The components are implemented with HTML templates, TypeScript files, and unit test files. The HTML templates contain the necessary markup for displaying the respective components. The components are also imported and used in the main component.
    
    Based on the recent user commits and repository commits, the commit message follows the convention of starting with a type (feat for a new feature) and providing a concise description of the changes made.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { TapComponent } from '../tap/tap.component';
+import { ActionsComponent } from '../actions/actions.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, TapComponent, ActionsComponent],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
